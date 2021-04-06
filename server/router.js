e3b71e024e86ebd0042066091c245814a8269aa1
leo_sportich <leo_sportich@ens.univ-artois.fr>
2021-04-06 16:48:13 +0200

    FEAT : Reach/route avec intégration dans React

--- server/router.js
+++ server/router.js
@@ -6,20 +6,31 @@ module.exports = router;
 
 router
     .get("/", (req, res) => {
-        res.json("Hello world!!");
+        res.json("Bienvenue sur le projet WEKA 2021 du groupe Les Crackheads De Lens !");
     });
 
 router
     .get('/quiz',
         async (req, res) => {
             const result = await db.query('select * from quiz');
+            console.log(result.rows);
             res.json(result.rows);
         })
 
     .get('/quiz/:id',
         async (req, res) => {
             const result = await db.query('select * from quiz where quiz_id =$1', [req.params.id]);
-            res.json(result.rows);
+            const question = await db.query('select ques_id from question where quiz_id =$1',[req.params.id]);
+            console.log([result.rows, question.rows]);
+            res.json([result.rows, question.rows]);
+        })
+
+    .get('/quiz/:id/:ques_id',
+        async (req, res) => {
+            const result = await db.query('select * from question where quiz_id =$1 and ques_id=$2', [req.params.id,req.params.ques_id]);
+            const proposition = await db.query('select * from proposition where ques_id =$1',[req.params.ques_id]);
+            console.log([result.rows,proposition.rows]);
+            res.json([result.rows,proposition.rows]);
         })
 
 
