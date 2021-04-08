467252c924de869cc77e81240f4807b994d8d69f
leo_sportich <leo_sportich@ens.univ-artois.fr>
2021-04-08 11:27:46 +0200

    WIP: Affichage des bonnes propositions et questions

--- src/Questions.js
+++ src/Questions.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 
+let score = 0;
+
 function Question(props) {
     return (
         <>
@@ -14,8 +16,8 @@ function Question(props) {
                 {props.propositions.map(p =>
                     <tr>
                         <td>
-                            {p.prop_phrase}
-                            <img className="image" src={p.prop_image} alt=""/>
+                            <input type="checkbox" id="proposition" name="proposition" value={p.prop_valide}/>
+                                <label htmlFor="subscribeNews">{p.prop_phrase}<img className="image" src={p.prop_image} alt=""/></label>
                         </td>
                     </tr>
                 )}
@@ -29,6 +31,7 @@ function Questions(props) {
     const [questions, setQuestions] = useState([]);
     const [propositions, setPropositions] = useState([]);
     const [currIndex, setCurrIndex] = useState(0);
+    const [initial, setInitial] = useState(true);
 
     useEffect(() => {
         const getQuestions = async () => {
@@ -46,31 +49,64 @@ function Questions(props) {
         )
 
     let q = document.getElementById("questions");
-    let score = 0;
 
-    const currentQuestion = questions[currIndex];
+    console.log('avant question : '+currIndex)
+    const currentQuestion = questions[currIndex]; // A voir si bien appelé
     const jsxQuestion = <Question question={currentQuestion.ques_phrase} points={currentQuestion.ques_points} propositions={propositions}/>;
 
     async function getPropositions() {
         let p = [];
+        console.log('getPropositions')
         const ques_id = currentQuestion.ques_id;
-        try{
-            p = (await axios.get('http://localhost:8000/quiz/'+ques_id+'/propositions')).data;
+        //console.log(ques_id)
+        //try{ // execute 2 fois ?
+            console.log('avant')
+        if(!initial){
+            await axios.get('http://localhost:8000/quiz/'+ques_id+'/propositions')
+            .then(response => {
+                p=response.data;
+                setPropositions(p);
+                setCurrIndex(currIndex+1)
+                console.log('result')})
+            .catch(error => {
+                console.log(error.response)
+            })
             console.log(p);
-        }catch (err){
-            alert(err)
-        }finally {
-            setPropositions(p);
+            // setPropositions(p);
+            console.log('apres')
+        }else{
+            await axios.get('http://localhost:8000/quiz/1/propositions')
+                .then(response => {
+                    p=response.data;
+                    setPropositions(p);
+                    //setCurrIndex(currIndex+1)
+                    console.log('result')})
+                .catch(error => {
+                    console.log(error.response)
+                })
+            setInitial(false)
         }
+
+            //
+            // console.log(currIndex)
+        //}catch (err){
+            //alert(err)
+        //}
     }
 
-    if(propositions.length===0){
+    if(propositions.length===0 && initial){
+        console.log('initial')
         getPropositions();
+        // let p = [{"prop_id":1,"prop_phrase":"Toto","prop_image":"","prop_valide":true,"ques_id":1},{"prop_id":2,"prop_phrase":"Cleveland","prop_image":"","prop_valide":true,"ques_id":1},{"prop_id":3,"prop_phrase":"Leo","prop_image":"","prop_valide":true,"ques_id":1}]
+        // setPropositions(p)
+        //setCurrIndex(currIndex+1)
+        //console.log(currIndex)
+        setInitial(false)
     }
 
     function handleClick() {
-        if (currIndex + 1 < questions.length){
-            setCurrIndex(currIndex + 1);
+        if (currentQuestion.ques_id < questions.length){
+            // setCurrIndex(currIndex + 1);
             getPropositions();
         }
         else q.innerHTML = "<h3 style='color: #ff0000'>Il n'y a plus de questions !</h3><br>" +
